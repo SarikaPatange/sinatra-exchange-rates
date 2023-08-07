4a1ad3b46357e4fc0a9e059ff4c8e7fee6b2ef38
Sarika Patange <128057504+SarikaPatange@users.noreply.github.com>
2023-08-07 22:16:03 +0000

    updated

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
+require "json"
+require 'http'
+symbols_url = "https://api.exchangerate.host/symbols"
+response_symbols = HTTP.get(symbols_url)
+response_symbols_hash = JSON.parse(response_symbols)
+@all_symbols = response_symbols_hash.fetch("symbols")
+@all_symbols.delete("BOB")
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @all_symbols = response_symbols_hash.fetch("symbols")
+  erb(:home)
+end
+
+get("/:symbol") do
+  @all_symbols = response_symbols_hash.fetch("symbols")
+  erb(:currencytoconvert)
+end
+
+get("/:symbol/:symbol2") do
+  @all_symbols = response_symbols_hash.fetch("symbols")
+  erb(:convert)
 end
